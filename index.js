a2e7440ff85f4a8a5645f0e91079f6b1768b142e
flooyd <floydtjones@gmail.com>
2018-02-26 18:59:29 +0000

    added comments in index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 $(() => {
+  
+  /*
+  Thank you to whoever grades this :)
+  
+  See bottom for starting point/handler initiation
+  I think I may have benefited from working with multiple files
+  such as one for functions related to favorites, one for handlers, etc.,
+  but that is something I will possibly implement while doing portfolio cleanup after
+  completing all capstones
+  */
 
   //temp storage for photos to be used to retrieve info if a photo is favorited
   let photos = [];
@@ -23,9 +33,10 @@ $(() => {
     total_photos: '124550'
   }];
 
-  //due to the way the queries work, I won't be allowing user to select camera for now (too many api calls)
-  //but I submitted an issue on the maintainer's github repo so maybe I will update this later :D
-  //cameras in rover objects also won't be used, but I will keep it there for potential update later
+  /*due to the way the queries work, I won't be allowing user to select camera for now (too many api calls)
+  cameras in rover objects also won't be used, but I will keep it there for potential update later
+  I submitted a feature request on maintainer's repo, and perhaps after I finish the course, I can learn some
+  Ruby on Rails and make a PR myself...*/
   let cameras = [
     'FHAZ-Front Hazard Avoidance Camera',
     'RHAZ-Rear Hazard Avoidance Camera',
@@ -37,7 +48,8 @@ $(() => {
     'PANCAM-Panoramic Camera',
     'MINITES-Miniature Thermal Emission Spectrometer'
   ];
-
+  
+  //I would like to eventually come back to this after finishing the course and add a back end to hide the API key
   getURI = () => {
     let date = $('#date').val();
     let rover = $('#rover').val();
@@ -56,6 +68,9 @@ $(() => {
         markFavorites();
       }
     }).fail(() => {
+      /*Unfortunately, the rate limit is given as a response header, not in the response itself.
+      I'm not sure if the header can be accessed through code (without server side), so I kind of have
+      guess for this error message */
       $('.error').html('API issue - possible rate limit - check header of request');
       toggleHide($('.error'), false);
       $('form button').prop('disabled', false);
@@ -64,6 +79,8 @@ $(() => {
 
   renderImages = (images, bFavorites) => {
     $('#results-js').empty();
+    //There might be a better way to handle this than essentially just repeating code, but the favorites have more data from the object
+    //from the API listed since it's out of context of the search
     if (bFavorites) {
       images.forEach(i => {
         $('#results-js').append(
@@ -98,7 +115,10 @@ $(() => {
       })
     }
   }
-
+  
+  /*The API has a very limited rate (1000/hour), so I store the results of the mainfest call in local storage, and update only once per hour.
+  Of course this can be "hacked" by deleting the item from local storage, but it's the best thing I could think of until I start working on the
+  server side.*/
   getManifest = rover => {
     let lastManifestTime = localStorage.getItem('manifestTime');
 
@@ -166,8 +186,8 @@ $(() => {
     populateRoverSummary(initialRover);
   }
 
-  //this is also functionality that won't be used now, but I will keep it here in case
-  //maintainer of api updates it to allow for easier camera search
+  /*this is also functionality that won't be used now, but I will keep it here in case
+  maintainer of api updates it to allow for easier camera search*/
   setCameraOptions = rover => {
     $('#camera').empty();
     rover = rovers.find(r => r.rover === rover);
@@ -176,7 +196,9 @@ $(() => {
       $('#camera').append($("<option>").val(camera).html(camera));
     })
   }
-
+  
+  /*more local storage. I wonder if it would be best practice to notify users that local storage is being used, in case they want
+  to clear their browser's history but keep their favorites. Probably not necessary for this app, but it's something I thought of*/
   saveFavorite = photoID => {
     let photo = photos.find(p => p.id == photoID);
     let favorites = JSON.parse(localStorage.getItem('favorites'));
@@ -199,7 +221,8 @@ $(() => {
     localStorage.setItem('favorites', JSON.stringify(newFavorites));
   }
 
-  //button will show that the photo is favorited (if hovered over). need more styling in future
+  /*button will show that the photo is favorited (if hovered over). need more styling in future.
+  Ideally, I would add a little heart or something to show that it's favorited, but I wanted to move on from this project for now.*/
   markFavorites = () => {
     let favorites = JSON.parse(localStorage.getItem('favorites'));
     $('.container button').each((i, button) => {
@@ -229,7 +252,8 @@ $(() => {
       }
     });
   };
-
+  
+  //notice the excessive calls to toggleHide...see my comment on the toggleHide function
   handleFavLinkClicked = () => {
     $('#favLink').click(() => {
       toggleHide('.intro', true);
@@ -290,6 +314,14 @@ $(() => {
       $('#results-js').empty();
       $('form button').prop('disabled', true);
       selectedRover = rovers.find(r => r.rover === $('#rover').val());
+      /* I asked for feedback in the flex slack channel, and a couple of people didn't like this. They suggested
+      changing the date automatically if a user picks an invalid date (based on landing date and latest photo date from manifest).
+      I spoke with my mentor and we agreed that automatically changing someone's input would be more confusing
+      than an error message, so I decided to stick with this approach
+      
+      Question for reviewer if you have a chance :) -> what is the "best practice" for long comments like this in real, production code?
+      Is it okay to have a comment in the function block itself, or should it be above the function?
+      */
       if ($(date).val() > selectedRover.max_date || $(date).val() < selectedRover.landing_date) {
         $('.error').html(`${selectedRover.rover} does not have photos for that date. She is not a time traveler. Change your date please.`);
         toggleHide($('.error'), false);
@@ -299,7 +331,12 @@ $(() => {
       }
     })
   }
-
+  
+  /* You'll notice this function is used quite often. Unfortunately, parts of my code are very "spaghetti-like,"
+  especially after I added the third page (favorites). I spoke with my mentor about this, and we agreed that this is a
+  side effect of JQuery only applications, and I'm confident I'll be able to handle it better with
+  things like React Router or actually delivering multiple pages from the server, with a template for all pages for
+  stuff like the header and footer */
   toggleHide = (target, hide) => {
     if (hide) {
       $(target).addClass('hide');
@@ -322,10 +359,11 @@ $(() => {
   handleFavLinkClicked();
 
 
-  //for testing
-  //toggleHide('.intro', true);
-  //toggleHide('.content', false);
-  //toggleHide('form', false)
-  //bOnHomePage = false;
-  //populateBrowse();
+  /*for testing - uncomment to always start on the browse page. I haven't tested the test since adding
+  the favorites page, so it might not function correctly, in which case you'll just have to start on the home page :)
+  toggleHide('.intro', true);
+  toggleHide('.content', false);
+  toggleHide('form', false)
+  bOnHomePage = false;
+  populateBrowse();*/
 })
